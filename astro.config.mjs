f6bd09f3ce88911b6f83f88bc1098ef4a18d0996
edis.eubov <edis.eubov@codexio.bg>
2026-07-27 13:03:37 +0300

    Prepare Astro + Keystatic for Vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import markdoc from '@astrojs/markdoc';
 import node from '@astrojs/node';
 import keystatic from '@keystatic/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic()],
-	adapter: node({ mode: 'standalone' }),
+    integrations: [react(), markdoc(), keystatic()],
+    adapter: vercel(),
 });
