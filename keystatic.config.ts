de5c46ec640da1efc7ee5c331ec0b0bcac311983
edis.eubov <edis.eubov@codexio.bg>
2026-07-27 14:37:53 +0300

    Fix inline post images for production

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -28,7 +28,7 @@ export default config({
 					options: {
 						image: {
 							directory: 'src/assets/images/posts',
-							publicPath: '/src/assets/images/posts/',
+							publicPath: '@assets/images/posts/',
 						},
 					},
 				}),
